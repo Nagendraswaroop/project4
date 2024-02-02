bb3eea395f0021d2d75899a733fb43654d61a00b
SaiTeja <saitejadandu6@gmail.com>
2024-02-02 11:44:45 +0530

    new c

---
+++ project4/Controllers/jjss.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace project4.Controllers
+{
+    public class jjss : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
